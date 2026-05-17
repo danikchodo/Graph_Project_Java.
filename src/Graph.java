dd6a536beba2a5cea3736e89b9c27e5377e6139f
danikchodo <01205862@pw.edu.pl>
2026-05-17 22:37:10 +0200

    Create Graph.java
    stworzenie struktury danych dla grapha

---
+++ src/Graph.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Graph {
+
+    public static class Node {
+        public int id;
+        public double x, y;
+
+        public Node(int id, double x, double y) {
+            this.id=id;
+            this.x=x;
+            this.y=y;
+        }
+    }
+
+    public static class Edge {
+        public int source_id;
+        public int dest_id;
+        public double weight;
+
+        public Edge(int source_Id,int dest_Id,double weight) {
+            this.source_id=source_id;
+            this.dest_id=dest_id;
+            this.weight=weight;
+        }
+    }
+
+    private List<Node>nodes=new ArrayList<>();
+    private List<Edge>edges=new ArrayList<>();
+
+    public void addNode(Node node) { nodes.add(node); }
+    public void addEdge(Edge edge) { edges.add(edge); }
+
+    public List<Node> getNodes() { return nodes; }
+    public List<Edge> getEdges() { return edges; }
+}

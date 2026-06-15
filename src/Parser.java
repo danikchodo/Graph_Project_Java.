51d230951d2c819445fa65482b131fdb461e1b62
danikchodo <01205862@pw.edu.pl>
2026-06-15 07:20:43 +0200

    Update Parser.java
    zmiana logiki parsera

--- src/Parser.java
+++ src/Parser.java
@@ -1,51 +1,31 @@
+package src;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.Locale;
 
-public interface Parser{
-    void load(File file,Graph graph)throws IOException;
-}
 
-class TxtParser implements Parser {
-    @Override
+public class Parser {
     public void load(File file, Graph graph) throws IOException {
         graph.getNodes().clear();
-        graph.getEdges().clear();
 
         try (Scanner sc=new Scanner(file)) {
         sc.useLocale(Locale.US);
-            if (!sc.hasNextInt()) 
-                return;
-            int numNodes=sc.nextInt();
-            if (!sc.hasNextInt())
-                return;
-            int numEdges=sc.nextInt();
-
-        for (int i = 0;i<numNodes;i++) {
-                if (!sc.hasNextInt()) break;
-                int id=sc.nextInt();
+        while (sc.hasNext()) {
+                if (!sc.hasNextInt()) {
+                    sc.next();
+                    continue;
+                }
+                int id = sc.nextInt();
                 
                 if (!sc.hasNextDouble()) break;
-                double x=sc.nextDouble();
+                double x = sc.nextDouble();
                 
                 if (!sc.hasNextDouble()) break;
-                double y=sc.nextDouble();
+                double y = sc.nextDouble();
                 
-                graph.addNode(new Graph.Node(id,x,y));
-            }
-
-            for (int i=0;i<numEdges;i++) {
-                if (!sc.hasNextInt()) break;
-                int src=sc.nextInt();
-                
-                if (!sc.hasNextInt()) break;
-                int dst=sc.nextInt();
-                
-                if (!sc.hasNextDouble()) break;
-                double weight=sc.nextDouble();
-                
-                graph.addEdge(new Graph.Edge(src,dst,weight));
+                graph.addNode(new Graph.Node(id, x, y));
             }
         }
     }
